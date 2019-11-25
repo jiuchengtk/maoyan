2f00d436a057fc63a8a75171b4c1285101577e0e
jiucheng <15279232381@163.com>
2019-11-25 18:32:42 +0800

    create Components

---
+++ src/components/CitySwitch/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>全城</div>
+</template>
+
+<script>
+export default {
+    name: 'CityBranch'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="main">
+        <Header title="猫眼影院"/>
+        <div id="content">
+            <div class="cinema_menu">
+				<router-link tag="li"  to="/cinema/citySwitch" class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</router-link>
+				<router-link tag="li" to="/cinema/brandSwitch" class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</router-link>
+				<router-link tag="li" to="/cinema/featureSwitch" class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</router-link>
+                <keep-alive>
+                    <router-view />
+                </keep-alive>
+			</div>
+            <CinemaList />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+import CinemaList from '@/components/CinemaList';
+
+export default {
+    name: 'mine',
+    components: {
+        Header,
+        TabBar,
+        CinemaList
+    }
+}
+</script>
+
+<style>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+#content .cinema_menu.router-link-active{  color: #ef4238; border-bottom: 2px #ef4238 solid; }
+
+</style>
